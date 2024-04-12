8b3c2c945f98cfd0a1a001b16f8f3464a5db6a73
Irrisorr <zakharkevichg@gmail.com>
2024-04-12 14:10:46 +0200

    Completed 21th task

--- 3_application/form_check.js
+++ 3_application/form_check.js
@@ -1,23 +1,24 @@
 function validate(form) {
-    if (!checkStringAndFocus(form.elements["f_imie"], "Podaj imię!")) { return false; }
-    if (!checkStringAndFocus(form.elements["f_nazwisko"], "Podaj nazwisko!")) { return false; }
-    if (!checkEmail(form.elements["f_email"])) { return false; }
-    if (!checkStringAndFocus(form.elements["f_kod"], "Podaj kod!")) { return false; }
-    if (!checkStringAndFocus(form.elements["f_ulica"], "Podaj ulicę!")) { return false; }
-    if (!checkStringAndFocus(form.elements["f_miasto"], "Podaj miasto!")) { return false; }
+    if (!checkStringAndFocus(form.elements["f_imie"], "Podaj imię!", isWhiteSpaceOrEmpty)) {return false;}
+    if (!checkStringAndFocus(form.elements["f_nazwisko"], "Podaj nazwisko!", isWhiteSpaceOrEmpty)) {return false;}
+    if (!checkStringAndFocus(form.elements["f_email"], "Podaj właściwy e-mail!", isEmailInvalid)) {return false;}
+    if (!checkStringAndFocus(form.elements["f_kod"], "Podaj kod!", isWhiteSpaceOrEmpty)) {return false;}
+    if (!checkStringAndFocus(form.elements["f_ulica"], "Podaj ulicę!", isWhiteSpaceOrEmpty)) {return false;}
+    if (!checkStringAndFocus(form.elements["f_miasto"], "Podaj miasto!", isWhiteSpaceOrEmpty)) {return false;}
     return true;
 }
 
 
-function checkStringAndFocus(obj, msg) {
+function checkStringAndFocus(obj, msg, func) {
     let str = obj.value;
     let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
-    if (isWhiteSpaceOrEmpty(str)) {
+    if (func(str)) {
         document.getElementById(errorFieldName).innerHTML = msg;
         obj.focus();
         return false;
     }
     else {
+        document.getElementById(errorFieldName).innerHTML = "";
         return true;
     }
 }
@@ -28,12 +29,12 @@ function isWhiteSpaceOrEmpty(str) {
 }
 
 
-function checkEmail(str) {
+function isEmailInvalid(str) {
     let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
-    if (email.test(str))
-        return true;
-    else {
-        alert("Podaj właściwy e-mail");
+    if (email.test(str)) {
         return false;
+    }
+    else {
+        return true;
     }
 }
