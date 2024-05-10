79e168deb4c0a7eacba82027596ad2fdda1f6655
Irrisorr <zakharkevichg@gmail.com>
2024-05-10 15:02:14 +0200

    completed 12th task

---
+++ 6_application/www/php_2.local/public_html/index.php
@@ -0,0 +1,19 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PHP</title>
+    <meta charset='UTF-8' />
+</head>
+
+<body>
+    <?php
+    echo "<h1>Nasz system hello</h1>";
+
+    //    echo "<p>Przesłany login: " . $login . "<br>";
+    //    echo "Przesłane hasło: " . $password . "</p>";
+    ?>
+</body>
+
+</html>
